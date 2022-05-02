e116d9ad4d48f45cf1f41c8267d80c664f309fd4
Bea Dobruski <bbeadobruski@gmail.com>
2022-05-02 18:33:21 -0300

    added: lib headless ui: popover to accessibility

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -1,31 +1,19 @@
 import { ChatTeardropDots } from 'phosphor-react';
-import { useState } from 'react';
+import { Popover } from '@headlessui/react';
 
 export function Widget() {
-  const [isWidgetOpen, setIsWidgetOpen] = useState(false);
-
-  function toggleWidgetVisibility() {
-    setIsWidgetOpen(!isWidgetOpen);
-    // valor atual: se está aberto ou não e vai SETAR
-    // o valor contrário dele
-  }
-
   return (
-    <div className="absolute bottom-5 right-5">
-      {isWidgetOpen && <p>Hello Houston.</p>}
-      {/*  se isWidgetOpen for TRUE, só assim ele executa a 2nd parte. ===> && (2x E comercial)*/}
+    <Popover className="absolute bottom-5 right-5">
+      <Popover.Panel>Hey, supp!</Popover.Panel>
 
-      <button
-        onClick={toggleWidgetVisibility}
-        className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group"
-      >
+      <Popover.Button className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">
         <ChatTeardropDots className="w-6 h-6" />
 
         <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear">
           <span className="pl-2"></span>
           Feedback
         </span>
-      </button>
-    </div>
+      </Popover.Button>
+    </Popover>
   );
 }
