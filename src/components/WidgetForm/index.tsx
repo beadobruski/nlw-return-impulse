0d6df3e8fd5e0cdca4a49a6eb9cafa83ee247ed1
Bea Dobruski <bbeadobruski@gmail.com>
2022-05-06 14:11:43 -0300

    relocating my components

---
+++ src/components/WidgetForm/index.tsx
@@ -0,0 +1,64 @@
+import { useState } from 'react';
+
+import { CloseButton } from '../CloseButton';
+import { FeedbackTypeStep } from './Steps/FeedbackTypeStep';
+import { FeedbackContentStep } from './Steps/FeedbackContentStep';
+
+import bugImageUrl from '../../assets/bug.svg';
+import ideaImageUrl from '../../assets/idea.svg';
+import thoughtImageUrl from '../../assets/thought.svg';
+
+export const feedbackTypes = {
+  BUG: {
+    title: 'Problema',
+    image: {
+      source: bugImageUrl,
+      alt: 'Imagem de um inseto com a cor roxa',
+    },
+  },
+  IDEA: {
+    title: 'Ideia',
+    image: {
+      source: ideaImageUrl,
+      alt: 'Imagem de uma lâmpada acesa',
+    },
+  },
+  OTHER: {
+    title: 'Outro',
+    image: {
+      source: thoughtImageUrl,
+      alt: 'Imagem de um balão de pensamento com a cor azul',
+    },
+  },
+};
+
+export type FeedbackType = keyof typeof feedbackTypes;
+
+export function WidgetForm() {
+  const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
+
+  return (
+    <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
+      <header>
+        <span className="text-xl leading-6">Deixe seu feedback</span>
+        <CloseButton />
+      </header>
+
+      {!feedbackType ? (
+        <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
+      ) : (
+        <FeedbackContentStep />
+      )}
+
+      <footer className="text-xs text-neutral-400">
+        Feito com ♥ pela{' '}
+        <a
+          className="underline underline-offset-2"
+          href="https://rocketseat.com.br/"
+        >
+          Rocketseat
+        </a>
+      </footer>
+    </div>
+  );
+}
